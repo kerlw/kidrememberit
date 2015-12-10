0e22872efe17ebfb1fdcc384966fbfd9f408dadb
kerlw <kerl.d.w@gmail.com>
2015-12-10 16:00:35 +0800

    reimplemented the puzzle generation algorithm

--- Classes/Puzzle.cpp
+++ Classes/Puzzle.cpp
@@ -33,7 +33,7 @@ bool PuzzleData::init(const UserData* usr) {
 	type = PuzzleType::PT_REMEMBER_AND_REPRESENT; //(PuzzleType) (usr->score / SCORE_RANK);
 
 	size = 5 + usr->score / 8;
-	card_types = usr->score % 8;
+	card_types = 2 + usr->score % 8;
 	if (size > 0) {
 		data = new uint8_t[size];
 		memset(data, 0, size);
@@ -56,8 +56,33 @@ bool Puzzle::generate() {
 	if (!m_pData)
 		return false;
 
-	for (int i = 0; i < m_pData->size; i++)
-		m_pData->data[i] = rand() % CARD_TYPE_COUNT;
+	std::set<uint8_t> types;
+	std::vector<uint8_t> types_v;
+	for (int i = 0; i < m_pData->card_types; ) {
+		uint8_t type = rand() % CARD_TYPE_COUNT;
+		if (types.find(type) != types.end())
+			continue;
 
+		types.insert(type);
+		types_v.push_back(type);
+		i++;
+	}
+
+	std::set<int> indexes;
+	for (int i = 0; i < m_pData->card_types; ) {
+		int index = rand() % m_pData->size;
+		if (indexes.find(index) != indexes.end())
+			continue;
+
+		m_pData->data[i] = types_v[i];
+		indexes.insert(index);
+		i++;
+	}
+
+	for (int i = 0; i < m_pData->size; i++) {
+		if (indexes.find(i) != indexes.end())
+			continue;
+		m_pData->data[i] = types_v[(rand() % m_pData->card_types)];
+	}
 	return true;
 }
